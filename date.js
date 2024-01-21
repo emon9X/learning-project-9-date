af8d53a1adfc26b167db49410adfe1f0831ee35c
emon9X <shahariar.emn990@gmail.com>
2024-01-21 19:10:05 +0600

    displaying date methods

--- date.js
+++ date.js
@@ -82,3 +82,38 @@ new Date(milliseconds)
 }
 
 //Date method
+
+
+//displaying date
+
+//toString()
+//JavaScript will (by default) output dates using the toString() method.
+{
+    const d = new Date("02-13-2022");
+    console.log(d);
+    d.toString();
+    console.log(d);
+}
+
+//toDateString()
+//more readable format
+//doesn't change the old date object. creates new date object
+{
+    const d = new Date();
+    const newD = d.toDateString();
+    console.log(newD);
+}
+
+//toUTCString()
+{
+    const d = new Date();
+    const newD = d.toUTCString();
+    console.log(newD);
+}
+
+//toISOString()
+{
+    const d = new Date();
+    const newD = d.toISOString();
+    console.log(newD);
+}
